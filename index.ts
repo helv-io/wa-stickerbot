663861470f4597cfafa530dfa30ddc0bbbda8215
Helvio Pedreschi <helvio88@gmail.com>
2021-02-11 23:28:39 -0500

    +- success tags

--- index.ts
+++ index.ts
@@ -58,8 +58,8 @@ const start = (client: Client) => {
               videoOpts.endTime = `00:00:${i.toString().padStart(2, '0')}.0`;
               try {
                 client.sendMp4AsSticker(message.from, dataURL, videoOpts, meta).then(
-                  s => console.log('sendMp4AsSticker', s),
-                  e => console.log('sendMp4AsSticker', e)
+                  s => console.log('+sendMp4AsSticker', s),
+                  e => console.log('-sendMp4AsSticker', e)
                 );
                 break;
               } catch {
@@ -70,8 +70,8 @@ const start = (client: Client) => {
             // Sends as Image sticker
             console.log('IMAGE Sticker', filename);
             client.sendImageAsSticker(message.from, dataURL).then(
-              s => console.log('sendImageAsSticker', s),
-              e => console.log('sendImageAsSticker', e)
+              s => console.log('+sendImageAsSticker', s),
+              e => console.log('-sendImageAsSticker', e)
             );
           }
         },
@@ -84,29 +84,29 @@ const start = (client: Client) => {
   const onMsg = client.onMessage(m);
 
   onMsg.then(
-    () => console.log('onMessage'),
-    e => console.log('onMessage', e)
+    () => console.log('+onMessage'),
+    e => console.log('-onMessage', e)
   );
 
   onAnyMsg.then(
-    () => console.log('onAnyMessage'),
-    e => console.log('onAnyMessage', e)
+    () => console.log('+onAnyMessage'),
+    e => console.log('-onAnyMessage', e)
   );
 
   // Click "Use Here" when another WhatsApp Web page is open
   client.onStateChanged(state => {
     if(state === "CONFLICT" || state === "UNLAUNCHED") {
       client.forceRefocus().then(
-        () => console.log('forceRefocus'),
-        e => console.log('forceRefocus', e));
+        () => console.log('+forceRefocus'),
+        e => console.log('-forceRefocus', e));
     }
   }).then(
-    () => console.log('onStateChanged'),
-    e => console.log('onStateChanged', e)
+    () => console.log('+onStateChanged'),
+    e => console.log('-onStateChanged', e)
   );
 };
 
 create(config).then(client => start(client)).then(
-  () => console.log('create'),
-  n => console.log('create', n)
+  () => console.log('+create'),
+  n => console.log('-create', n)
 );
