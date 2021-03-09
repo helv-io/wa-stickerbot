0c4bfa1472feacd2eb48bfc2ea3dfa6a4cd0f93a
Helvio Pedreschi <helvio88@gmail.com>
2021-03-09 18:54:52 -0500

    production env

--- ecosystem.config.sample.js
+++ ecosystem.config.sample.js
@@ -8,7 +8,8 @@ module.exports = {
             "IMGFLIP_USERNAME": "YOUR_IMGFLIP_USERNAME",
             "IMGFLIP_PASSWORD": "YOUR_IMGFLIP_PASSWORD",
             "TENOR_API": "YOUR_TENOR_API_KEY",
-            "GIPHY_API": "YOUR_GIPHY_API_KEY"
+            "GIPHY_API": "YOUR_GIPHY_API_KEY",
+            "NODE_ENV": "production"
           }
         }
     ]
