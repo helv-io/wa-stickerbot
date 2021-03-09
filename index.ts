b295b3d55625d5742fa12f02ba87acd958711304
Helvio Pedreschi <helvio88@gmail.com>
2021-03-08 23:03:02 -0500

    DEBUG

--- index.ts
+++ index.ts
@@ -80,7 +80,7 @@ const start = (client: Client) => {
         void await client.sendImageAsSticker(message.from, dataURL, meta);
       }
     } else {
-      console.log('Text Message');
+      console.log('Text Message', message.body);
       const keyword = message.body.toLowerCase().match(/sticker d[a|e|o]s? (.*)/);
       if(keyword) {
         const { data: gifs }  = await giphy.search(keyword[1], giphyOptions);
