d6203be7552966f66c8ddc4327d0027b450b5310
Helvio Pedreschi <helvio88@gmail.com>
2021-03-09 22:44:30 -0500

    Prettier Code

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # wa-stickerbot
+
 WhatsApp Sticker Bot. Any images, GIFs or Videos you receive will be converted to a Sticker!
 
 ### Pre Requisites
+
 1. Ubuntu or another Debian distribution.
 2. NodeJS:
-`curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
-sudo apt install -y nodejs`
+   `curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash - sudo apt install -y nodejs`
 3. ffmpeg:
-`sudo apt install ffmpeg`
+   `sudo apt install ffmpeg`
 4. A phone with WhatsApp installed
 5. pm2 Package Manager `sudo npm install -g pm2`
 
 ### Downloading the source
+
 1. `cd /opt`
 2. `git clone https://github.com/Helvio88/wa-stickerbot`
 3. `cd wa-stickerbot`
---
+++ types/Giphy.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+export interface GiphySearch {
+  api_key: string;
+  lang?: string;
+  limit: number;
+  q: string;
+  type?: string;
+}
+
+export interface GiphyResponse {
+  data: GiphyGif[];
+}
+
+export interface GiphyGif {
+  type: string;
+  id: string;
+  slug: string;
+  url: string;
+  bitly_url: string;
+  embed_url: string;
+  username: string;
+  source: string;
+  rating: string;
+  content_url: string;
+  source_tld: string;
+  source_post_url: string;
+  update_datetime: string;
+  create_datetime: string;
+  import_datetime: string;
+  trending_datetime: string;
+  title: string;
+  images: GiphyImages;
+}
+
+export interface GiphyImages {
+  original: GiphyImageDefinition;
+  fixed_width: GiphyImageDefinition;
+}
+
+export interface GiphyImageDefinition {
+  webp: string;
+  webp_size: string;
+}
