fb3f839fb86ba45f9c5945c8693ec23692acf461
Helvio Pedreschi <helvio88@gmail.com>
2021-03-13 11:59:07 -0500

    fix paths

--- utils/giphyHandler.ts
+++ utils/giphyHandler.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { GiphyResponse, GiphySearch } from 'types/Giphy'
+import { GiphyResponse, GiphySearch } from '../types/Giphy'
 
 const giphyBaseUrl = 'https://api.giphy.com/v1'
 
