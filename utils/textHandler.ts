4ef79869d568ff280e43640240d4c7447fd533d8
Helvio Pedreschi <helvio88@gmail.com>
2021-05-13 14:46:01 -0400

    Refresh Web Option

--- utils/textHandler.ts
+++ utils/textHandler.ts
@@ -3,15 +3,16 @@ import { normalize } from 'normalize-diacritics'
 export const stickerRegExp = /(sticker|figurinha)(s?) d[a|e|o]s? (.*)/i
 
 export const getTextAction = async (message: string) => {
-  message = await normalize(message.toLowerCase())
+  if (message) {
+    message = await normalize(message.toLowerCase())
 
-  if (message === 'memes') return actions.MEME_LIST
-  if (message === 'link') return actions.LINK
-  if (message === 'instrucoes' || message === 'rtfm')
-    return actions.INSTRUCTIONS
-  if (stickerRegExp.exec(message)) return actions.STICKER
-  if (message.startsWith('meme ')) return actions.MEME
-  return null
+    if (message === 'memes') return actions.MEME_LIST
+    if (message === 'link') return actions.LINK
+    if (message === 'instrucoes' || message === 'rtfm')
+      return actions.INSTRUCTIONS
+    if (stickerRegExp.exec(message)) return actions.STICKER
+    if (message.startsWith('meme ')) return actions.MEME
+  }
 }
 
 export enum actions {
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,5 +1,45 @@
+import { Client } from '@open-wa/wa-automate'
 import qs from 'qs'
+
+import { botOptions } from '../config'
+import { getImgflipImage } from './imgflipHandler'
 
 export const paramSerializer = (p: any) => {
   return qs.stringify(p, { arrayFormat: 'brackets' })
+}
+
+export const registerParticipantsListener = (client: Client) => {
+  client.onGlobalParticipantsChanged(async (event) => {
+    const groupId = event.chat as unknown as `${number}-${number}@g.us`
+
+    switch (event.action) {
+      case 'remove': {
+        console.log('Removed', event.who)
+        if (botOptions.interactOut) {
+          client.sendImage(
+            groupId,
+            await getImgflipImage(botOptions.outMessage),
+            '',
+            `Adeus +${event.who.toString().split('@')[0]}, vai tarde!`
+          )
+        }
+        break
+      }
+
+      case 'add': {
+        console.log('Added', event.who)
+        if (botOptions.interactOut) {
+          client.sendImage(
+            groupId,
+            await getImgflipImage(botOptions.inMessage),
+            '',
+            `Divirta-se, +${event.who.toString().split('@')[0]}!`
+          )
+          const groupInfo = await client.getGroupInfo(groupId)
+          client.sendText(groupId, groupInfo.description)
+        }
+        break
+      }
+    }
+  })
 }
