8f0755c5fe6dd505c61d43b15cfc3ef30b2cdcb6
Helvio Pedreschi <helvio88@gmail.com>
2022-06-13 10:39:40 -0400

    Use native decrypter

--- utils/mediaHandler.ts
+++ utils/mediaHandler.ts
@@ -1,18 +1,4 @@
-import { decryptMedia } from '@open-wa/wa-decrypt'
-import mime from 'mime-types'
 import { mp4StickerConversionOptions } from '../config'
-
-export const getMedia = async (message: any) => {
-  const mediaData = await decryptMedia(message)
-  const media: WhatsappMedia = {
-    filename: `${message.t}.${mime.extension(message.mimetype || '') || ''}`,
-    mediaData: mediaData,
-    dataURL: `data:${message.mimetype || ''};base64,${mediaData.toString(
-      'base64'
-    )}`
-  }
-  return media
-}
 
 export const getConversionOptions = (duration: number) => {
   const cOptions = mp4StickerConversionOptions
