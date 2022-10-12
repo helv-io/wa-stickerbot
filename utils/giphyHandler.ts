76be6e88cd0a5e126d9005e58715087829fc9a73
Helvio Pedreschi <helvio88@gmail.com>
2022-10-12 19:21:23 +0000

    try-catch for Tenor and Gihpy

--- utils/giphyHandler.ts
+++ utils/giphyHandler.ts
@@ -5,26 +5,31 @@ const giphyBaseUrl = 'https://api.giphy.com/v1'
 
 export const getGiphys = async (search: GiphySearch) => {
   if (!search.api_key) return []
-  const giphys = (
-    await axios.get<GiphyResponse>(`${giphyBaseUrl}/gifs/search`, {
-      params: search
-    })
-  ).data.data.concat(
-    (
-      await axios.get<GiphyResponse>(`${giphyBaseUrl}/stickers/search`, {
+  try {
+    const giphys = (
+      await axios.get<GiphyResponse>(`${giphyBaseUrl}/gifs/search`, {
         params: search
       })
-    ).data.data
-  )
-
-  const urls: string[] = []
-  giphys.forEach((giphy) =>
-    urls.push(
-      (giphy.images.original.webp_size <= '1400000'
-        ? giphy.images.original.webp
-        : giphy.images.fixed_width.webp
-      ).replace(/media[0-9]/, 'i')
+    ).data.data.concat(
+      (
+        await axios.get<GiphyResponse>(`${giphyBaseUrl}/stickers/search`, {
+          params: search
+        })
+      ).data.data
     )
-  )
-  return urls
+
+    const urls: string[] = []
+    giphys.forEach((giphy) =>
+      urls.push(
+        (giphy.images.original.webp_size <= '1400000'
+          ? giphy.images.original.webp
+          : giphy.images.fixed_width.webp
+        ).replace(/media[0-9]/, 'i')
+      )
+    )
+    return urls
+  } catch (e) {
+    console.error(e)
+    return []
+  }
 }
--- utils/tenorHandler.ts
+++ utils/tenorHandler.ts
@@ -5,19 +5,24 @@ const tenorURL = 'https://g.tenor.com/v1/search'
 
 export const getTenors = async (search: TenorSearch) => {
   if (!search.key) return []
-  const tenors = (
-    await axios.get<TenorResponse>(tenorURL, {
-      params: search
-    })
-  ).data.results
+  try {
+    const tenors = (
+      await axios.get<TenorResponse>(tenorURL, {
+        params: search
+      })
+    ).data.results
 
-  const urls: string[] = []
-  tenors.forEach((tenor) =>
-    urls.push(
-      tenor.media[0].gif.size <= 1400000
-        ? tenor.media[0].gif.url
-        : tenor.media[0].tinygif.url
+    const urls: string[] = []
+    tenors.forEach((tenor) =>
+      urls.push(
+        tenor.media[0].gif.size <= 1400000
+          ? tenor.media[0].gif.url
+          : tenor.media[0].tinygif.url
+      )
     )
-  )
-  return urls
+    return urls
+  } catch (e) {
+    console.error(e)
+    return []
+  }
 }
