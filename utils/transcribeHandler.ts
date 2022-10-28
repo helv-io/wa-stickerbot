cc7b6ce7569f5e9121c3c58b63ceacf0a9234c5d
Helvio Pedreschi <helvio88@gmail.com>
2022-10-28 05:07:36 +0000

    Should end recognition

--- utils/transcribeHandler.ts
+++ utils/transcribeHandler.ts
@@ -1,32 +1,31 @@
 import {
   AudioConfig,
-  Recognizer,
   SpeechConfig,
-  SpeechRecognitionEventArgs,
   SpeechRecognizer
 } from 'microsoft-cognitiveservices-speech-sdk'
 import { botOptions } from '../config'
 import fs from 'fs/promises'
 
-export const transbribeAudio = async (wav: string, transcript = '') => {
+export const transbribeAudio = async (wav: string) => {
   const sConfig = SpeechConfig.fromSubscription(botOptions.azureKey, 'eastus')
   sConfig.speechRecognitionLanguage = botOptions.azureLanguage
   const aConfig = AudioConfig.fromWavFileInput(await fs.readFile(wav))
   const reco = new SpeechRecognizer(sConfig, aConfig)
+  const transcript: string[] = []
 
   reco.recognizing = (_sender, event) => {
     console.log('recognizing', event.result)
-    transcript += event.result.text
+    transcript.push(event.result.text)
   }
 
   reco.recognized = (_sender, event) => {
     console.log('recognized', event.result)
-    transcript += event.result.text
+    reco.stopContinuousRecognitionAsync()
     reco.close()
   }
 
   reco.startContinuousRecognitionAsync()
 
   console.log('Completed Transcription')
-  return transcript
+  return transcript.join(' ')
 }
