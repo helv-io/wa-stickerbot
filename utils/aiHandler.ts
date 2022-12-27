9dd71fb1d7ac2f40c5baedd1db3c4d355e668c8c
Helvio Pedreschi <helvio88@gmail.com>
2022-12-27 17:18:49 +0000

    No ChatGPT yet

--- utils/aiHandler.ts
+++ utils/aiHandler.ts
@@ -3,27 +3,10 @@ import { ChatGPTAPI } from 'chatgpt-commonjs'
 
 const org = process.env.OPENAI_API_ORG
 const key = process.env.OPENAI_API_KEY
-const session = process.env.SB_OPENAI_SESSION
-const clearance = process.env.SB_OPENAI_CLEARANCE
-const userAgent = process.env.SB_USERAGENT
-let api: ChatGPTAPI
-if (session && clearance && userAgent) {
-  api = new ChatGPTAPI({
-    sessionToken: session,
-    clearanceToken: clearance,
-    userAgent: userAgent,
-    markdown: false
-  })
-}
+
 
 export const ask = async (question: string, user: string) => {
-  if (session && clearance && userAgent) {
-    if (!(await api.getIsAuthenticated())) {
-      await api.ensureAuth()
-    }
-    const response = await api.sendMessage(question, { conversationId: user })
-    return response
-  } else if (org && key) {
+  if (org && key) {
     const completionRequest: CreateCompletionRequest = {
       model: 'code-davinci-002',
       prompt: `//Human readable\nQ: ${question}\nA:`,
