0ef2642e2af5d2206127f45ab9b6fe3be0428613
Helvio Pedreschi <helvio88@gmail.com>
2023-02-09 14:34:12 -0500

    Stars not good.

--- src/utils/baileysHelper.ts
+++ src/utils/baileysHelper.ts
@@ -21,7 +21,7 @@ export const makeSticker = async (message: WAMessage, url = '') => {
   if (!url) {
     const buffer = <Buffer>await downloadMediaMessage(message, 'buffer', {})
 
-    const types = [StickerTypes.DEFAULT, StickerTypes.CIRCLE, StickerTypes.ROUNDED, StickerTypes.STAR]
+    const types = [StickerTypes.DEFAULT, StickerTypes.CIRCLE, StickerTypes.ROUNDED]
 
     for (const type of types) {
       const sticker = new Sticker(buffer, {
