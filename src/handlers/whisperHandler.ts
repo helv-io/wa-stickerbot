78c27a8c766726ba60e5dd813d8527c690051363
Helvio Pedreschi <helvio88@gmail.com>
2024-03-12 04:58:36 +0000

    Added http headers to ASR call

--- src/handlers/whisperHandler.ts
+++ src/handlers/whisperHandler.ts
@@ -6,7 +6,16 @@ export const transcribeAudio = async (filename: string, data: string) => {
     const form = new FormData()
     const blob = new Blob([Buffer.from(data, 'base64')])
     form.append('audio_file', blob, filename)
-    const response = await axios.post(`${botOptions.whisperEndpoint}/asr`, form)
-    console.debug(response)
-    return await response.data
+    try {
+        const response = await axios.post(`${botOptions.whisperEndpoint}/asr`, form, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+        console.log(response)
+        return await response.data
+    } catch (e) {
+        console.error(e)
+        return 'Womp Womp Womp'
+    }
 }
