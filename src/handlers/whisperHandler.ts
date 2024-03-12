2479ce0753e4189dda09ab9b35b6c8d771c18a97
Helvio Pedreschi <helvio88@gmail.com>
2024-03-12 19:04:48 +0000

    auto detect language for translation

--- src/handlers/whisperHandler.ts
+++ src/handlers/whisperHandler.ts
@@ -10,11 +10,22 @@ export const transcribeAudio = async (filename: string, data: string) => {
   return (await axios.post<string>(`${botOptions.whisperEndpoint}/asr`, form, { headers })).data
 }
 
-export const translateAudio = async (filename: string, data: string, lang: string = 'en') => {
+export const translateAudio = async (filename: string, data: string) => {
+  let lang = await detectLanguage(filename, data)
+  lang = lang === 'en' ? 'pt' : 'en'
   const form = new formdata()
   form.append('audio_file', Buffer.from(data, 'base64'), filename)
   const headers = {'Content-Type': 'multipart/form-data'}
   return (await axios.post<string>(
     `${botOptions.whisperEndpoint}/asr?task=translate&language=${lang}`,
     form, { headers })).data
+}
+
+const detectLanguage = async (filename: string, data: string) => {
+  const form = new formdata()
+  form.append('audio_file', Buffer.from(data, 'base64'), filename)
+  const headers = {'Content-Type': 'multipart/form-data'}
+  return (
+    await axios.post<{detected_language: string, language_code: string}>
+    (`${botOptions.whisperEndpoint}/detect-language`, form, { headers })).data.language_code
 }
